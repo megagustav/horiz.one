fc08cd901b182af7196d31e968d0165177920bcc
Gustav <g.moorhouse@me.com>
2022-12-08 12:26:02 +0000

    dynamically add class to body & change active nav

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,20 @@
-const navItems = document.querySelectorAll('.nav-link');
-
-for (let i = 0; i < navItems.length; i++) {
-	navItems[i].addEventListener("click", function() {
-		Array.from(navItems, navItem => navItem.classList.remove('active'));
-		navItems[i].classList.add('active');
-	});
+function pageID() {
+		var pageID = document.querySelector('.page').id;
+		console.log(pageID);
+		document.body.classList.add(pageID);
 }
+
+function matchPageIDwithNav() {
+	var navID = document.querySelector('.nav-link').id;
+	var pageID = document.querySelector('.page').id;
+	var currentNav = document.querySelector('nav ul li a#' + pageID);
+	var currentNavID = currentNav.id;
+	
+	if(currentNavID = pageID) {
+		console.log("navID matches pageID:" + currentNavID);
+		document.querySelector('nav ul li a#' + pageID).classList.add('active');
+	}
+}
+
+pageID()
+matchPageIDwithNav()
